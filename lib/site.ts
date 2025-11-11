05cf50c7d7619d089890308c7f76e866a560a878
genekellyboyle <DrDuffy@bhhsnv.com>
2025-11-10 21:04:07 -0800

    chore: update site contact phone number

--- lib/site.ts
+++ lib/site.ts
@@ -1,6 +1,6 @@
 export const CONTACT_EMAIL = 'DrDuffySells@SunstoneWoodsideHomes.com'
-export const CONTACT_PHONE = '1-702-957-1926'
-export const CONTACT_PHONE_LINK = 'tel:+17029571926'
+export const CONTACT_PHONE = '(702) 766-7950'
+export const CONTACT_PHONE_LINK = 'tel:+17027667950'
 
 export const CONTACT_ADDRESS = '10249 Celestial Pole St, Las Vegas, NV 89143'
 export const SALES_HOURS = 'Mon 2–5 PM • Tue–Sun 10 AM–5 PM'
